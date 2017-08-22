c07c1d471b5d45e22ea1b8b1f63fce8463b99558
Luis Espinola <lespinola@gmail.com>
2017-08-22 19:21:06 -0400

    Created auth custom route

---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const Auth = ({component: Component, path, loggedIn}) => (
+  <Route path={path} render={(props) => (
+    !loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/" />
+    )
+  )}/>
+);
+
+const mapStateToProps = state => {
+  return {loggedIn: Boolean(state.session.currentUser)};
+};
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
