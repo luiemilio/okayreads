e125cdbe920d587ba7e6cf956a33b4c21429e62e
Luis Espinola <lespinola@gmail.com>
2017-08-22 08:49:14 -0400

    Created users api views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
-    resources :users, only: [:create]
+    resources :users, only: [:create, :show]
   end
 end
