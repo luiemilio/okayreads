d5d3239fc0ab6b0c48cc1094cea43b006bd97b23
Luis Espinola <lespinola@gmail.com>
2017-08-22 20:30:42 -0400

    Created Book model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id          :integer          not null, primary key
+#  title       :string           not null
+#  author      :string           not null
+#  image_url   :string           not null
+#  publisher   :string           not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Book < ApplicationRecord
+   validates :title, :author, :image_url,
+             :publisher, :description, presence: true
+
+    
 end
